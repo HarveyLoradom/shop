21f000a98cadca0e8dd69d8591ed6cd8395c05e6
HarveyLoradom <3044024900@qq.com>
2026-05-03 16:03:30 +0800

    主页和Tab栏

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/Cart/index.dart';
+import 'package:shop/pages/Category/index.dart';
+import 'package:shop/pages/Home/index.dart';
+import 'package:shop/pages/Mine/index.dart';
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
 
@@ -7,11 +11,63 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String,String>> _tabList=[{
+    "icon":"lib/assets/ic_public_home_normal.png",
+    "active_icon":"lib/assets/ic_public_home_active.png",
+    "text":"首页",
+  },
+  {
+    "icon":"lib/assets/ic_public_pro_normal.png",
+    "active_icon":"lib/assets/ic_public_pro_active.png",
+    "text":"分类",
+  },
+  {
+    "icon":"lib/assets/ic_public_cart_normal.png",
+    "active_icon":"lib/assets/ic_public_cart_active.png",
+    "text":"购物车",
+  },
+  {
+    "icon":"lib/assets/ic_public_my_normal.png",
+    "active_icon":"lib/assets/ic_public_my_active.png",
+    "text":"我的",
+  }];
+
+  int _currentIndex=0;
+  List<Widget> _getChildren(){
+    return [HomeView(),CategoryView(),CartView(),MineView()];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("主页1"),),
-      body: Center(child: Text("主页"),),
+      body: SafeArea(child:IndexedStack(
+        index:_currentIndex,
+       children: _getChildren(), 
+      )),
+      bottomNavigationBar:BottomNavigationBar(
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.black,
+        items: _getTabBarWidget(),
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          _currentIndex=index;
+          setState(() { });
+        },
+        
+        ),
     );
   }
+
+  List<BottomNavigationBarItem> _getTabBarWidget(){
+  return List.generate(_tabList.length, (int index){
+    return BottomNavigationBarItem(
+      icon: Image.asset(_tabList[index]["icon"]!,width: 30,height: 30,),
+      activeIcon: Image.asset(_tabList[index]["active_icon"]!,width: 30,height: 30,),
+      label: _tabList[index]["text"],
+      );
+  });
+ }
 }
+
+ 
