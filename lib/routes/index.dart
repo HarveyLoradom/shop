be5d6e19a29b1dcfd2f79c475a228a49c99821a5
HarveyLoradom <3044024900@qq.com>
2026-05-03 15:18:52 +0800

    搭建基础组件和路由

---
+++ lib/routes/index.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shop/pages/Login/index.dart';
+import 'package:shop/pages/Main/index.dart';
+
+Widget getRootWidget(){
+  return MaterialApp(
+    initialRoute: "/",
+    routes: getRootRoutes(),
+  );
+}
+
+
+Map<String, Widget Function(BuildContext)> getRootRoutes(){
+  return {
+    "/":(context)=>MainPage(),
+    "/login":(context)=>LoginPage(),
+  };
+}
