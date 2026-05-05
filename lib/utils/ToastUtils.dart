c25bc000b4aa8ba54b0fea84f16d9e7124d1e0ad
HarveyLoradom <3044024900@qq.com>
2026-05-05 08:50:36 +0800

    登录页面搭建

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  static bool showLoading = false;
   static void showToast(BuildContext context,String msg){
+    if(showLoading){
+      return;
+    }
+    showLoading = true;
+    Future.delayed(Duration(seconds: 3),(){
+      showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 120,
+        width: 180,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(40),
         ),
         behavior: SnackBarBehavior.floating,
-        duration: Duration(seconds: 1),
+        duration: Duration(seconds: 3),
         content: Text(msg ?? "加载成功",textAlign: TextAlign.center,),
       ),
     );
