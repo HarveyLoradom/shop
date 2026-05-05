4b63553bfb620d522a5e64151b52772d11c58ad0
HarveyLoradom <3044024900@qq.com>
2026-05-05 14:31:07 +0800

    修改间距问题

--- lib/components/Home/HmSuggestion.dart
+++ lib/components/Home/HmSuggestion.dart
@@ -3,38 +3,44 @@ import 'package:shop/viewmodels/home.dart';
 
 class HmSuggestion extends StatefulWidget {
   final SpecialRecommendItem productList;
-  HmSuggestion({Key? key,required this.productList}) : super(key: key);
+  HmSuggestion({Key? key, required this.productList}) : super(key: key);
 
   @override
   _HmSuggestionState createState() => _HmSuggestionState();
 }
 
 class _HmSuggestionState extends State<HmSuggestion> {
-
-  List<GoodsItem> _getDisplayItems(){
-    if(widget.productList.subTypes.isEmpty){
+  List<GoodsItem> _getDisplayItems() {
+    if (widget.productList.subTypes.isEmpty) {
       return [];
     }
     return widget.productList.subTypes.first.goodsItems.items.take(3).toList();
   }
 
-  Widget _buildHeader(){
+  Widget _buildHeader() {
     return Row(
       children: [
-        Text("特惠推荐",style: TextStyle(
-          color: const Color.fromARGB(255, 39, 14, 14),
-          fontSize: 18,
-          fontWeight: FontWeight.w700),),
-        SizedBox(width: 10,),
-        Text("精选省攻略",style: TextStyle(
-          color: const Color.fromARGB(255, 50, 46, 46),
-          fontSize: 12),
+        Text(
+          "特惠推荐",
+          style: TextStyle(
+            color: const Color.fromARGB(255, 39, 14, 14),
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
         ),
-      ]
+        SizedBox(width: 10),
+        Text(
+          "精选省攻略",
+          style: TextStyle(
+            color: const Color.fromARGB(255, 50, 46, 46),
+            fontSize: 12,
+          ),
+        ),
+      ],
     );
   }
 
-  Widget _buildLeft(){
+  Widget _buildLeft() {
     return Container(
       width: 100,
       height: 140,
@@ -48,30 +54,41 @@ class _HmSuggestionState extends State<HmSuggestion> {
     );
   }
 
-  List<Widget> _getChildrenList(){
-    List <GoodsItem> list=_getDisplayItems();
-    return List.generate(list.length, (index){
-      return Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              errorBuilder: (context, error, stackTrace) => Image.asset("lib/assets/home_cmd_inner.png"),
-              list[index].picture,width: 100,height: 140,fit: BoxFit.cover),
-          ),
-          SizedBox(height:10),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 4),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.red,
+  List<Widget> _getChildrenList() {
+    List<GoodsItem> list = _getDisplayItems();
+    return List.generate(list.length, (index) {
+      return Expanded(
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                errorBuilder: (context, error, stackTrace) => Image.asset(
+                  "lib/assets/home_cmd_inner.png",
+                  //width: 100,
+                  height: 140,
+                  fit: BoxFit.cover,
+                ),
+                list[index].picture,
+                //width: 100,
+                height: 140,
+                fit: BoxFit.cover,
+              ),
             ),
-            child:Text("${list[index].price}",style: TextStyle(
-              color: Colors.white,
-),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.red,
+              ),
+              child: Text(
+                "${list[index].price}",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       );
     });
   }
@@ -80,7 +97,7 @@ class _HmSuggestionState extends State<HmSuggestion> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10),
-      child:Container(
+      child: Container(
         decoration: BoxDecoration(
           color: Colors.blue,
           borderRadius: BorderRadius.circular(12),
@@ -94,21 +111,23 @@ class _HmSuggestionState extends State<HmSuggestion> {
         child: Column(
           children: [
             _buildHeader(),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             Row(
-              children:[
+              children: [
                 _buildLeft(),
+                SizedBox(width: 10),
                 Expanded(
                   child: Row(
+                    spacing: 10,
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: _getChildrenList(),
                   ),
                 ),
-              ]
-            )
+              ],
+            ),
           ],
         ),
-      )
+      ),
     );
-  }  
+  }
 }
